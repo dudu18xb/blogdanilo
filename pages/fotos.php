f352a5328fb51477cd47bf2f26379284ae65f217
Eduardo Rocha <eduardorocha460@gmail.com>
2019-04-01 22:12:46 -0300

    ajustes tela de galeria de imagens e fotos

--- pages/fotos.php
+++ pages/fotos.php
@@ -6,60 +6,61 @@
         <li>Fotos</li>
     </ul>
 
-    <div class="container titulopadrao">
-        <legend><h1 id="demo08" class="wow fadeInLeft"><i class="fa fa-camera" aria-hidden="true"></i> Fotos</h1></legend>
-        <div class="hover08 column">
-            <div>
-                <figure class="wow fadeInLeft">
-                    <a class="example-image-link" href="images/img_1.jpg" data-lightbox="example-1">
-                        <img class="example-image" src="images/img_1.jpg" alt="olho" />
+    <div class="container">
+        <div class="row services-row hover08 column wow fadeIn">
+            <legend>
+                <h2 id="demo08" class="text-center"><i class="fa fa-camera" aria-hidden="true"></i> Galeria de Fotos</h2>
+            </legend>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+                <figure>
+                    <a class="example-image-link" href="images/home_1.jpg" data-lightbox="example-1">
+                        <img class="example-image" src="images/home_1.jpg" alt="banner" />
                     </a>
                 </figure>
-                <span>Olho Azul</span>
+                <span>Banner</span>
+
             </div>
-            <div>
-                <figure class="wow fadeIn">
-                    <a class="example-image-link" href="images/img_2.jpg" data-lightbox="example-1">
-                        <img class="example-image" src="images/img_2.jpg" alt="Transformes" />
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+                <figure>
+                    <a class="example-image-link" href="images/home_2.jpg" data-lightbox="example-1">
+                        <img class="example-image" src="images/home_2.jpg" alt="banner 2" />
                     </a>
                 </figure>
-                <span>Transformes</span>
+                <span>Banner 1</span>
             </div>
-            <div>
-                <figure class="wow fadeInRight">
-                    <a class="example-image-link" href="images/img_3.jpg" data-lightbox="example-1">
-                        <img class="example-image" src="images/img_3.jpg" alt="carro" />
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+                <figure>
+                    <a class="example-image-link" href="images/home3_3.jpg" data-lightbox="example-1">
+                        <img class="example-image" src="images/home_3.jpg" alt="viagem" />
                     </a>
                 </figure>
-                <span>Carro</span>
+                <span>Viagem</span>
             </div>
-        </div>
-        <br>
-        <div class="hover08 column">
-            <div>
-                <figure class="wow fadeInLeft">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+                <figure>
                     <a class="example-image-link" href="images/img_4.jpg" data-lightbox="example-1">
                         <img class="example-image" src="images/img_4.jpg" alt="terra" />
                     </a>
                 </figure>
                 <span>Terra</span>
             </div>
-            <div>
-                <figure class="wow fadeIn">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+                <figure>
                     <a class="example-image-link" href="images/img_5.jpg" data-lightbox="example-1">
                         <img class="example-image" src="images/img_5.jpg" alt="moto" />
                     </a>
                 </figure>
                 <span>Moto</span>
             </div>
-            <div>
-                <figure class="wow fadeInRight">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+                <figure>
                     <a class="example-image-link" href="images/img_6.png" data-lightbox="example-1">
                         <img class="example-image" src="images/img_6.png" alt="esperanca" />
                     </a>
                 </figure>
                 <span>Em busca da felicidade</span>
             </div>
+
         </div>
     </div>
-</div>
+</div> 
--- pages/home.php
+++ pages/home.php
@@ -36,61 +36,60 @@
     <!-- final do carrousel -->
 </div>
 <div class="container">
-    <div class="row services-row">
-        <legend><h1 id="demo08" class="wow fadeInLeft"><i class="fa fa-camera" aria-hidden="true"></i>Life</h1></legend>
-        <div class="hover08 column">
-            <div>
-                <figure class="wow fadeInLeft">
-                    <a class="example-image-link" href="images/home_1.jpg" data-lightbox="example-1">
-                        <img class="example-image" src="images/home_1.jpg" alt="banner"/>
-                    </a>
-                </figure>
-                <span>Banner</span>
-            </div>
-            <div>
-                <figure class="wow fadeInDown">
-                    <a class="example-image-link" href="images/home_2.jpg" data-lightbox="example-1">
-                        <img class="example-image" src="images/home_2.jpg" alt="banner 2"/>
-                    </a>
-                </figure>
-                <span>Banner 1</span>
-            </div>
-            <div>
-                <figure class="wow fadeInRight">
-                    <a class="example-image-link" href="images/home3_3.jpg" data-lightbox="example-1">
-                        <img class="example-image" src="images/home_3.jpg" alt="viagem"/>
-                    </a>
-                </figure>
-                <span>Viagem</span>
-            </div>
+    <div class="row services-row hover08 column wow fadeIn">
+        <legend>
+        <h2 id="demo08" class="text-center"><i class="fa fa-camera" aria-hidden="true"></i> Galeria de Fotos</h2>
+        </legend>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/home_1.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/home_1.jpg" alt="banner" />
+                </a>
+            </figure>
+            <span>Banner</span>
+
         </div>
-        <br>
-        <div class="hover08 column">
-            <div>
-                <figure class="wow fadeInLeft">
-                    <a class="example-image-link" href="images/img_4.jpg" data-lightbox="example-1">
-                        <img class="example-image" src="images/img_4.jpg" alt="terra"/>
-                    </a>
-                </figure>
-                <span>Terra</span>
-            </div>
-            <div>
-                <figure class="wow fadeInUp">
-                    <a class="example-image-link" href="images/img_5.jpg" data-lightbox="example-1">
-                        <img class="example-image" src="images/img_5.jpg" alt="moto"/>
-                    </a>
-                </figure>
-                <span>Moto</span>
-            </div>
-            <div>
-                <figure class="wow fadeInRight">
-                    <a class="example-image-link" href="images/img_6.png" data-lightbox="example-1">
-                        <img class="example-image" src="images/img_6.png" alt="esperanca"/>
-                    </a>
-                </figure>
-                <span>Em busca da felicidade</span>
-            </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/home_2.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/home_2.jpg" alt="banner 2" />
+                </a>
+            </figure>
+            <span>Banner 1</span>
         </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/home3_3.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/home_3.jpg" alt="viagem" />
+                </a>
+            </figure>
+            <span>Viagem</span>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/img_4.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/img_4.jpg" alt="terra" />
+                </a>
+            </figure>
+            <span>Terra</span>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/img_5.jpg" data-lightbox="example-1">
+                    <img class="example-image" src="images/img_5.jpg" alt="moto" />
+                </a>
+            </figure>
+            <span>Moto</span>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-30">
+            <figure>
+                <a class="example-image-link" href="images/img_6.png" data-lightbox="example-1">
+                    <img class="example-image" src="images/img_6.png" alt="esperanca" />
+                </a>
+            </figure>
+            <span>Em busca da felicidade</span>
+        </div>
+
     </div>
 </div>
-</div>
+</div> 
